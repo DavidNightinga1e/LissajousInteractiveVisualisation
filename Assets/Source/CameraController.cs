d22764b897f1c0e0828b1a8c4247547aaef11e61
David Nightingale <nightrider2552@gmail.com>
2022-04-19 19:45:45 +0300

    camera update, UI fixes

---
+++ Assets/Source/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lissajous
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+
+        [Range(0, 100)] public float xSensitivity; 
+        [Range(0, 100)] public float ySensitivity; 
+        
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(1))
+            {
+                transform.RotateAround(target.position, target.up, Input.GetAxis("Mouse X") * Time.deltaTime * xSensitivity);
+                transform.RotateAround(target.position, transform.right, -Input.GetAxis("Mouse Y") * Time.deltaTime * ySensitivity);
+            }
+        }
+    }
+}
---
+++ Assets/Source/CanvasHider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lissajous
+{
+    [RequireComponent(typeof(Canvas), typeof(CanvasGroup))]
+    public class CanvasHider : MonoBehaviour
+    {
+        [SerializeField] private Camera camera;
+        
+        private CanvasGroup _canvasGroup;
+        private Canvas _canvas;
+
+        private void Awake()
+        {
+            _canvas = GetComponent<Canvas>();
+            _canvasGroup = GetComponent<CanvasGroup>();
+            if (!camera)
+                camera = Camera.main;
+        }
+
+        private void Update()
+        {
+            var dot = Vector3.Dot(camera.transform.position - _canvas.transform.position, _canvas.transform.forward);
+            var isHidden = dot >= 0;
+            _canvasGroup.alpha = isHidden ? 0f : 1f;
+        }
+    }
+}
